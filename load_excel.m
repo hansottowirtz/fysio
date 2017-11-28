ea4f8d5e632f4412be3fb1ae434568b89741e61f
eliashavermans <elias.havermans@student.uantwerpen.be>
2017-11-28 15:03:36 +0100

    load excel

--- load_excel.m
+++ load_excel.m
@@ -1,7 +1,12 @@
 function [ output_args ] = load_excel( input_args )
 %LOAD_EXCEL Summary of this function goes here
 %   Detailed explanation goes here
+[filename,pathname] = uigetfile('*.xlsx');
+disp(filename);
+disp(pathname);
 
+[num,txt,raw] = xlsread([pathname,filename]);
 
+disp(raw);
 end
 
