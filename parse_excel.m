cdaf70e6be211dde0dce2743193b9b95b812dfa3
Hans Otto Wirtz <hansottowirtz@gmail.com>
2017-11-29 11:28:40 +0100

    datasets parser

--- parse_excel.m
+++ parse_excel.m
@@ -1,15 +1,16 @@
 function [ description, frequency, datasets ] = parse_excel( raw )
     for i = 1:16
-        switch raw(i, 1)
+        switch raw{i, 1}
             case 'Description:'
                 description = raw(i, 2);
             case 'TRAJECTORIES'
                 frequency = raw(i+1, 1);
                 datasets_row = raw(i+2, :);
+                datasets = struct;
                 for j = 1:length(datasets_row)
-                    value = datasets_row{i};
-                    if ~isnan(value)
-                        value
+                    dataset_name = datasets_row{j};
+                    if ~isnan(dataset_name)
+                        datasets = setfield(datasets, dataset_name, []);
                     end
                 end
         end
