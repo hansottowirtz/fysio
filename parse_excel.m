9c1a29d83a6303b0995382cdf990de6cca732b5f
Hans Otto Wirtz <hansottowirtz@gmail.com>
2017-11-29 11:49:33 +0100

    Finish parse excel with structs

--- parse_excel.m
+++ parse_excel.m
@@ -2,15 +2,22 @@ function [ description, frequency, datasets ] = parse_excel( raw )
     for i = 1:16
         switch raw{i, 1}
             case 'Description:'
-                description = raw(i, 2);
+                description = raw{i, 2};
             case 'TRAJECTORIES'
-                frequency = raw(i+1, 1);
+                frequency = raw{i+1, 1};
                 datasets_row = raw(i+2, :);
                 datasets = struct;
+                values_starting_index = i+4;
                 for j = 1:length(datasets_row)
                     dataset_name = datasets_row{j};
                     if ~isnan(dataset_name)
-                        datasets = setfield(datasets, dataset_name, []);
+                        dataset_values = struct;
+                        dataset_values.n = cell2mat(raw(values_starting_index:end, 1));
+                        value_names = ['x', 'y', 'z'];
+                        for k = 0:2
+                            dataset_values.(value_names(k+1)) = cell2mat(raw(values_starting_index:end, j+k));
+                        end
+                        datasets.(dataset_name) = dataset_values;
                     end
                 end
         end
