65845b3c275a5c8cdd5169cd4ed5f21735f70e30
Hans Otto Wirtz <hansottowirtz@gmail.com>
2017-12-05 14:05:36 +0100

    Moar functions

--- plot_graphs.m
+++ plot_graphs.m
@@ -2,8 +2,16 @@ function [ output_args ] = plot_graphs( datasets )
 
 datasets_names = fieldnames(datasets);
 
-datasets_choices = cat(1, ['All'], datasets_names);
+datasets_choices = cat(1, 'All', datasets_names);
 dataset_choice_i = menu('Choose a dataset', datasets_choices);
+
+window_function_names = {'Bartlett',       'Blackman',       'Boxcar',        'Chebyshev',     'Hamming',       'Hann',       'Kaiser',       'Taylor',           'Triang'};
+window_functions =      {@(M) bartlett(M), @(M) blackman(M), @(M) rectwin(M), @(M) chebwin(M), @(M) hamming(M), @(M) hann(M), @(M) kaiser(M), @(M) taylorwin(M),  @(M) triang(M)};
+
+window_function_choice_i = menu('Choose a window function', window_function_names);
+window_function = window_functions{window_function_choice_i};
+
+disp(window_function);
 
 for i = 1:length(datasets_names)
     dataset_name = datasets_names{i};
@@ -17,23 +25,10 @@ for i = 1:length(datasets_names)
     else
         title('All');
     end
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
     rotate3d on
     hold on
 end
 hold off
-
-return;
-
-choice = menu('Choose a window function','Bartlett','Blackman','Boxcar','Chebyshev','Hamming','Hann','Kaiser','Taylor','Triang');
-switch choice
-    case 'Bartlett'
-    case 'Blackman'
-    case 'Boxcar'
-    case 'Chebyshev'
-    case 'Hamming'
-    case 'Hann'
-    case 'Kaiser'
-    case 'Taylor'
-    case 'Triang'
-end
-
