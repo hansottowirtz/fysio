411f3010934447f768c03bedd2b2b7531d1c7534
eliashavermans <elias.havermans@student.uantwerpen.be>
2017-12-19 15:18:40 +0100

    plot graphs

--- plot_graphs.m
+++ plot_graphs.m
@@ -14,23 +14,26 @@ function [ plotted_datasets ] = plot_graphs( datasets, window_function, handles 
         end
         plotted_datasets{end+1} = dataset_name;
         dataset_values = datasets.(dataset_name);
+        
+        t=0:0.01:200; A=sin(t);
+        
 
-        plot(handles.axes1, dataset_values.n, dataset_values.x);
+        plot(handles.axes1, t, A);
         title(handles.axes1, 'X');
         
-        semilogx(handles.axes4, dataset_values.n, log(fft(real(dataset_values.x))));
+        plot(handles.axes4, t, (fft(real(A))));
         title(handles.axes4,'fft X');
 
         plot(handles.axes2, dataset_values.n, dataset_values.y);
         title(handles.axes2, 'Y');
         
-        semilogx(handles.axes5, dataset_values.n, log(fft(real(dataset_values.y))));
+        semilogx(handles.axes5, 0:length(dataset_values.n), (fft(real(dataset_values.y))));
         title(handles.axes5,'fft Y');
 
         plot(handles.axes3, dataset_values.n, dataset_values.z);
         title(handles.axes3, 'Z');
         
-        semilogx(handles.axes6, dataset_values.n, log(fft(real(dataset_values.z))));
+        semilogx(handles.axes6, dataset_values.n, (fft(real(dataset_values.z))));
         title(handles.axes6,'fft Z');
         
         hold on;
